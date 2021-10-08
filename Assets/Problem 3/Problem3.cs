5db4102b25329fa93bf02facd016aa7dfe5e205c
RedHunter7 <adlirafif42121@gmail.com>
2021-10-08 14:55:42 +0700

    Problem 3

---
+++ Assets/Problem 3/Problem3.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Problem3 : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        rigidBody2D.AddForce(new Vector2(-50, 15));
+    }
+}
